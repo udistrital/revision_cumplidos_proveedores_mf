3c7bacd4ba3aa01be8bc071a0fa532d9f6c3a87c
stevencol <steven2088@hotmail.es>
2024-07-03 06:24:38 -0500

    feat: Crear el parcel según los lineamientos
    Se creó el parcel siguiendo los lineamientos establecidos, utilizando Node.js 18 y Angular 16.
    
    udistrital/Cumplidos_y_certificaciones_documentacion#70

---
+++ src/app/empty-route/empty-route.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Route } from '@angular/router';
+
+
+@Component({
+  selector: 'app-empty-route',
+  template: '',
+})
+export class EmptyRouteComponent {}
