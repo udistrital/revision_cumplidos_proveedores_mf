c985916b204a59518172f933ca652881963c9e4e
stevencol <steven2088@hotmail.es>
2024-08-28 15:19:27 -0500

    feat: Agregar nueva variable de entorno para el servicio CRUD de amanzon crud
    udistrital/Cumplidos_y_certificaciones_documentacion#124

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,6 +4,7 @@ export const environment = {
     CUMPLIDOS_PROVEEDORES_MID_SERVICE: 'http://localhost:8090/v1',
     CUMPLIDOS_PROVEEDORES_CRUD_SERVICE: 'http://localhost:8080/v1',
     DOCUMENTOS_CRUD:"http://pruebasapi.intranetoas.udistrital.edu.co:8094/v1/",
+    CORE_AMAZON_CRUD:"https://autenticacion.portaloas.udistrital.edu.co/apioas/core_amazon_crud/v1",
     FIRMA_ELECTRONICA_MID : "https://autenticacion.portaloas.udistrital.edu.co/apioas/firma_electronica_mid/v1",
     // Notificaciones
   NOTIFICACION_MID_WS: "ws://autenticacion.portaloas.udistrital.edu.co/apioas/notificacion_mid/v1/ws",
