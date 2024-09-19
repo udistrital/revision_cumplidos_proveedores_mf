a208ff7702341d64b336af36779386355aa1d8ca
stevencol <steven2088@hotmail.es>
2024-09-18 19:58:26 -0500

    feat: Modificación de card - Se modifica la card para recibir los botones mediante el componente padre y se recorren en el HTML con el fin de mostrar las funciones enviadas. udistrital/Cumplidos_y_certificaciones_documentacion#151

--- src/app/components/general-components/card-soporte/card-soporte.component.ts
+++ src/app/components/general-components/card-soporte/card-soporte.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, Input, numberAttribute, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  numberAttribute,
+  Output,
+} from '@angular/core';
 import { Soporte } from 'src/app/models/soporte.model';
 import { MatDialog } from '@angular/material/dialog';
 import { PopUpManager } from 'src/app/managers/popUpManager';
@@ -8,106 +14,111 @@ import { ComentarioSoporte } from 'src/app/models/basics/comentario-soporte.mode
 import { CumplidosProveedoresCrudService } from 'src/app/services/cumplidos_proveedores_crud.service';
 import { ModalVisualizarSoporteComponent } from '../modal-visualizar-soporte/modal-visualizar-soporte.component';
 import { CambioEstadoCumplido } from 'src/app/models/basics/cambio-estado-cumplio.model';
-import { ConfigSoportes, Mode,RolUsuario } from 'src/app/models/modal-soporte-cumplido-data.model';
+import {
+  ConfigSoportes,
+  Mode,
+  RolUsuario,
+} from 'src/app/models/modal-soporte-cumplido-data.model';
 import { SoporteCumplido } from 'src/app/models/soporte_cumplido.model';
-
-
+import { Button } from 'src/app/models/button.model';
 
 @Component({
   selector: 'app-card-soporte',
   templateUrl: './card-soporte.component.html',
-  styleUrls: ['./card-soporte.component.scss']
+  styleUrls: ['./card-soporte.component.scss'],
 })
 export class CardSoporteComponent {
-
-  @Input({required:true}) soporte!:SoporteCumplido
-  @Input({required:true}) config!:ConfigSoportes
-  @Input({required:true}) cambioEstadoCumplido!:CambioEstadoCumplido
+  @Input({ required: true }) soporte!: SoporteCumplido;
+  @Input({ required: true }) config!: ConfigSoportes;
+  @Input() bunttons!: Button[];
+  @Input({ required: true }) cambioEstadoCumplido!: CambioEstadoCumplido;
   @Output() recargarSoportes = new EventEmitter<any>();
-  comentario: string = "";
-  mode=Mode
-  rolUsuario=RolUsuario
+  comentario: string = '';
+  mode = Mode;
+  rolUsuario = RolUsuario;
 
   constructor(
     public dialog: MatDialog,
     private popUpManager: PopUpManager,
     private cumplidosMidServices: CumplidosProveedoresMidService,
-    private aletManagerService:AletManagerService,
-    private cumplidos_provedore_crud_service:CumplidosProveedoresCrudService,
-  ){
-
-  }
+    private aletManagerService: AletManagerService,
+    private cumplidos_provedore_crud_service: CumplidosProveedoresCrudService
+  ) {}
 
   ngOnInit() {
-    console.log("Soportes ", this.soporte)
-    console.log("cambio estado id",this.cambioEstadoCumplido)
+    console.log('Soportes ', this.soporte);
+    console.log('cambio estado id', this.cambioEstadoCumplido);
   }
 
-  openVerSoporte() {
-    this.dialog.open(ModalVisualizarSoporteComponent, {
-      disableClose: true,
-      height: '70vh',
-      width: '50vw',
-      maxWidth: '60vw',
-      maxHeight: '80vh',
-      panelClass: 'custom-dialog-container',
-      data: { url: this.soporte.Archivo.File }
-    });
-  }
-
-  async eliminarSoporte(){
- 
-       const confirm = await this.aletManagerService.alertConfirm("¿Deseas Eliminar el soporte?");
-       if(confirm.isConfirmed){
-        
-        console.log(this.soporte)
-        try{
-          this.cumplidosMidServices.delete(`/solicitud-pago/soportes`, this.soporte.Documento)
-        .subscribe({
-          next: (res: any) => {
-            this.recargarSoportes.emit(res)
-            this.popUpManager.showSuccessAlert('Soporte eliminado correctamente');
-          },
-          error: (error: any) => {
-            this.popUpManager.showErrorAlert('No fue posible eliminar el soporte');
-          }
-        });
-        }catch(error){
-            this.aletManagerService.showCancelAlert("Error","Se produjo"+error)
-        }
-        
-       }else{
-        this.aletManagerService.showCancelAlert("Cancelado","No se elimino")
-       }
-  }
-
-  async enviarComentario(){
-
-    await this.aletManagerService.alertConfirm("¿Estas seguro de enviar las observaciones?").then((confirmed: any)=>{
-
-      console.log(confirmed)
-
-      if(confirmed.isConfirmed){
-        console.log("Comentario",this.comentario)
-        try{
-          const  comentario:ComentarioSoporte= <ComentarioSoporte>{
-            SoporteCumplidoId: {
-              Id:this.soporte.SoporteCumplidoId
+  async eliminarSoporte() {
+    const confirm = await this.aletManagerService.alertConfirm(
+      '¿Deseas Eliminar el soporte?'
+    );
+    if (confirm.isConfirmed) {
+      console.log(this.soporte);
+      try {
+        this.cumplidosMidServices
+          .delete(`/solicitud-pago/soportes`, this.soporte.Documento)
+          .subscribe({
+            next: (res: any) => {
+              this.recargarSoportes.emit(res);
+              this.popUpManager.showSuccessAlert(
+                'Soporte eliminado correctamente'
+              );
             },
-            CambioEstadoCumplidoId:{
-              Id:this.cambioEstadoCumplido.Id
+            error: (error: any) => {
+              this.popUpManager.showErrorAlert(
+                'No fue posible eliminar el soporte'
+              );
             },
-            Comentario:this.comentario
-          }
-          this.cumplidos_provedore_crud_service.post("/comentario_soporte",comentario).subscribe((response)=>{
-            this.aletManagerService.showSuccessAlert("Comentario Guardado", "Se ha guardado el comentario en el documento")
           });
-        }catch(error){
-          this.aletManagerService.showCancelAlert("Cancelado", "No se ha guardo ningun comentario");
+      } catch (error) {
+        this.aletManagerService.showCancelAlert('Error', 'Se produjo' + error);
+      }
+    } else {
+      this.aletManagerService.showCancelAlert('Cancelado', 'No se elimino');
+    }
+  }
+
+  async enviarComentario() {
+    await this.aletManagerService
+      .alertConfirm('¿Estas seguro de enviar las observaciones?')
+      .then((confirmed: any) => {
+        console.log(confirmed);
+
+        if (confirmed.isConfirmed) {
+          console.log('Comentario', this.comentario);
+          try {
+            const comentario: ComentarioSoporte = <ComentarioSoporte>{
+              SoporteCumplidoId: {
+                Id: this.soporte.SoporteCumplidoId,
+              },
+              CambioEstadoCumplidoId: {
+                Id: this.cambioEstadoCumplido.Id,
+              },
+              Comentario: this.comentario,
+            };
+            this.cumplidos_provedore_crud_service
+              .post('/comentario_soporte', comentario)
+              .subscribe((response) => {
+                this.aletManagerService.showSuccessAlert(
+                  'Comentario Guardado',
+                  'Se ha guardado el comentario en el documento'
+                );
+              });
+          } catch (error) {
+            this.aletManagerService.showCancelAlert(
+              'Cancelado',
+              'No se ha guardo ningun comentario'
+            );
+          }
         }
-      } 
-    }).catch(()=>{
-      this.aletManagerService.showCancelAlert("Cancelado", "No se ha guardo ningun comentario");
-    });
-   }
+      })
+      .catch(() => {
+        this.aletManagerService.showCancelAlert(
+          'Cancelado',
+          'No se ha guardo ningun comentario'
+        );
+      });
+  }
 }
