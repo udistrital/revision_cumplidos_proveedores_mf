0fe8ef5202954e4e3a8a0f39c19568090d645381
faidercamilo26 <olayafaider@gmail.com>
2024-10-07 14:37:51 -0500

    feature: se cambian las variables de entorno para que se usen las de autenticacion

--- src/environments/environments.production.ts
+++ src/environments/environments.production.ts
@@ -5,9 +5,8 @@ export const environment = {
     'https://autenticacion.portaloas.udistrital.edu.co/apioas/revision_cumplido_proveedores_mid/v1',
     CUMPLIDOS_PROVEEDORES_CRUD_SERVICE: //'http://api.intranetoas.udistrital.edu.co:8104/v1'
     'https://autenticacion.portaloas.udistrital.edu.co/apioas/revision_cumplido_proveedores_crud/v1',
-    DOCUMENTOS_CRUD: 'http://pruebasapi.intranetoas.udistrital.edu.co:8094/v1/',
     CORE_AMAZON_CRUD:
-      'https://autenticacion.portaloas.udistrital.edu.co/apioas/core_amazon_crud/v1', 
+      'https://autenticacion.portaloas.udistrital.edu.co/apioas/core_amazon_crud/v1',
     FIRMA_ELECTRONICA_MID:
       'https://autenticacion.portaloas.udistrital.edu.co/apioas/firma_electronica_mid/v1',
       JBPM: 'http://busservicios.intranetoas.udistrital.edu.co:8282/wso2eiserver/services/administrativa_pruebas',
@@ -16,6 +15,5 @@ export const environment = {
     'http://pruebasapi2.intranetoas.udistrital.edu.co:8561',
    NOTIFICACION_MID_WS: "ws://autenticacion.portaloas.udistrital.edu.co/apioas/notificacion_mid/v1/ws",
     ADMINISTRATIVA_AMAZON_API: "http://api.intranetoas.udistrital.edu.co:8104/v1"
-   
+
   };
-  
