5d69d13bc7c42b0a5deab6b4ac9b7718b43422a6
stevencol <steven2088@hotmail.es>
2024-10-09 22:33:03 -0500

    feat: Agregar modelos de Mes y HistóricoCumplido
    Se añaden modelos para representar los objetos correspondientes.
    
    udistrital/Cumplidos_y_certificaciones_documentacion#183

---
+++ src/app/models/historico-cumplido.model.ts
@@ -0,0 +1,10 @@
+export interface HistoricoCumplido {
+    numeroContrato: string;
+    vigencia: string;
+    rp: string;
+    mes: string;
+    fechaAprobacion: Date;
+    nombreProveedor: string;
+    dependencias: string;
+    acciones: any;
+  }
