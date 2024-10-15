617cf2105035a2edc7c99184201799f108a94282
faidercamilo26 <olayafaider@gmail.com>
2024-10-15 07:55:09 -0500

    feature: Se elimina del modal de soportes cumplido la peticion de obtener el ultimo cambio estado.  udistrital/Cumplidos_y_certificaciones_documentacion#185

--- src/app/components/general-components/modal-comentarios-soporte/modal-comentarios-soporte.component.ts
+++ src/app/components/general-components/modal-comentarios-soporte/modal-comentarios-soporte.component.ts
@@ -3,6 +3,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { PopUpManager } from 'src/app/managers/popUpManager';
 import { InformacionComentarioIndividual } from 'src/app/models/informacion-comentario-individual';
 import { ModalComentariosSoporteData, Mode, RolUsuario } from 'src/app/models/modal-soporte-cumplido-data.model';
+import { CambioEstadoCumplido } from 'src/app/models/revision_cumplidos_proveedores_crud/cambio-estado-cumplio.model';
 import { ComentarioSoporte } from 'src/app/models/revision_cumplidos_proveedores_crud/comentario-soporte.model';
 import { CumplidosProveedoresCrudService } from 'src/app/services/cumplidos_proveedores_crud.service';
 
@@ -16,7 +17,7 @@ export class ModalComentariosSoporteComponent implements OnInit {
   comentarios: ComentarioSoporte[] = [];  // Inicializamos la lista de comentarios vacía
   informacionComentarioIndividual: InformacionComentarioIndividual[] = [];
   isLoading: boolean = false;  // Indicador para saber si estamos cargando los datos
-
+  cambioEstadoCumplido!:CambioEstadoCumplido;
   mode = Mode;
   rolUsuario = RolUsuario;
 
@@ -28,6 +29,7 @@ export class ModalComentariosSoporteComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.ObtenerUltimoCambioEstado();
     this.obtenerComentariosSoporte();
   }
 
@@ -53,6 +55,7 @@ export class ModalComentariosSoporteComponent implements OnInit {
               'Soporte sin comentarios',
               'Este soporte no tiene comentarios registrados.'
             );
+            this.isLoading = false;
           }
         },
         error: () => {
@@ -65,6 +68,25 @@ export class ModalComentariosSoporteComponent implements OnInit {
         }
       });
     }
+  }
+
+  ObtenerUltimoCambioEstado(){
+    this.isLoading = true;
+    console.log(this.cambioEstadoCumplido)
+    this.cumplidos_provedore_crud_service.get(`/cambio_estado_cumplido/?query=Activo:true,CumplidoProveedorId.Id:${this.data.CumplidoProveedorId}&sortby=FechaCreacion&order=desc`).subscribe({
+      next:(response)=>{
+        this.isLoading = false;
+        if(!response.Success || response.Data.length==0){
+          console.log(this.cambioEstadoCumplido)
+        }
+        this.cambioEstadoCumplido=response.Data[0]
+        console.log(response)
+      },
+      error: err=>{
+        console.log(err)
+        this.isLoading = false;
+      },
+    })
   }
 
   close(): void {
