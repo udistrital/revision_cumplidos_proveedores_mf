c81dd4672c12a49028d84c03b0c0156eb5f24e69
stevencol <steven2088@hotmail.es>
2024-10-17 02:55:26 -0500

    feat: Agregar funcionalidad para agregar ítems
    Se añade la funcionalidad para agregar ítems a la lista, así como para eliminarlos.
    
    udistrital/Cumplidos_y_certificaciones_documentacion#186

--- src/app/components/evaluacion-proveedor/gestion-evaluadores/items-a-evaluar/items-a-evaluar.component.ts
+++ src/app/components/evaluacion-proveedor/gestion-evaluadores/items-a-evaluar/items-a-evaluar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ItemAEvaluar } from './../../../../models/item_a_evaluar';
+import { PopUpManager } from 'src/app/managers/popUpManager';
 
 @Component({
   selector: 'app-items-a-evaluar',
@@ -9,8 +10,9 @@ import { ItemAEvaluar } from './../../../../models/item_a_evaluar';
 })
 export class ItemsAEvaluarComponent {
   formAddIntems: FormGroup;
+  numberId:number=0;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private popUpManager: PopUpManager) {
     this.formAddIntems = this.fb.group({
       detalles_item: [null, Validators.required],
     });
@@ -19,19 +21,39 @@ export class ItemsAEvaluarComponent {
 
   listaItems: ItemAEvaluar[] = [];
 
-  imprimir() {
-
-    this.listaItems.push({
-      id: this.listaItems.length + 1,
-      descripcion: this.obtenerInfoFormulario().item,
-    });
-    this.formAddIntems.reset();
-
+  async imprimir() {
+    if (this.obtenerInfoFormulario().item != '') {
+      let confirm = await this.popUpManager.showConfirmAlert(
+        '¿Estás seguro de agregar el ítem?'
+      );
+      if (confirm.isConfirmed) {
+        this.listaItems.push({
+          id: this.numberId+ 1,
+          descripcion: this.obtenerInfoFormulario().item,
+        });
+        this.numberId= this.numberId+ 1
+        this.formAddIntems.reset({
+          detalles_item: '',
+        });
+      }
+    }else{
+      this.popUpManager.showErrorAlert("La descripción no puede estar vacía");
+    }
   }
 
   obtenerInfoFormulario() {
     return {
       item: this.formAddIntems.get('detalles_item')?.getRawValue() ?? '',
     };
+  }
+
+  async eliminarItem(id:number) {
+    let confirm = await this.popUpManager.showConfirmAlert(
+      '¿Estás seguro de agregar el ítem?'
+    );
+    if(confirm){
+      this.listaItems = this.listaItems.filter(item=>item.id!==id)
+    }
+   
   }
 }
