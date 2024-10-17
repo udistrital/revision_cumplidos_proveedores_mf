139d0caf9af489940218aa40809a444986ed837f
stevencol <steven2088@hotmail.es>
2024-10-16 22:18:56 -0500

    fet: Crear vista pra gestion de evaluadores
    se crear un vista para el componente gestion evaluadores
    
    udistrital/Cumplidos_y_certificaciones_documentacion#186

--- src/app/components/evaluacion-proveedor/gestion-evaluadores/gestion-evaluadores.component.ts
+++ src/app/components/evaluacion-proveedor/gestion-evaluadores/gestion-evaluadores.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gestion-evaluadores',
   templateUrl: './gestion-evaluadores.component.html',
-  styleUrls: ['./gestion-evaluadores.component.scss']
+  styleUrls: ['./gestion-evaluadores.component.scss'],
 })
-export class GestionEvaluadoresComponent {
+export class GestionEvaluadoresComponent implements OnInit {
+  nombreProveedor!: string;
+  numeroContrato!: string;
+  vigenciaContrato!: string;
 
+  ngOnInit(): void {
+    this.nombreProveedor = 'nombre Proveedor  tempotal';
+    this.numeroContrato = 'numero de contrato tempotal';
+    this.vigenciaContrato = 'vigenciaContrato tempotal';
+  }
 }
