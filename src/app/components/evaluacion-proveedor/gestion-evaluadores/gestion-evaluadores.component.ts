a0c0d116dc579cafadd1ef4570134a8a3aa2a3cb
stevencol <steven2088@hotmail.es>
2024-10-16 20:19:54 -0500

    feat: Crear componente principal para gestión de evaluadores
    Se crea el componente principal para la vista de gestión de evaluadores.
    
    udistrital/Cumplidos_y_certificaciones_documentacion#186

---
+++ src/app/components/evaluacion-proveedor/gestion-evaluadores/gestion-evaluadores.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-evaluadores',
+  templateUrl: './gestion-evaluadores.component.html',
+  styleUrls: ['./gestion-evaluadores.component.scss']
+})
+export class GestionEvaluadoresComponent {
+
+}
