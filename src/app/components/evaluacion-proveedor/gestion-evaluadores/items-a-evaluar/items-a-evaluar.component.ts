2f07670b086169fa6ca5d542938d74f6d9827141
stevencol <steven2088@hotmail.es>
2024-10-16 20:21:17 -0500

    feat: Crear componente  de items
    Se crea el componente de items
    
    udistrital/Cumplidos_y_certificaciones_documentacion#186

---
+++ src/app/components/evaluacion-proveedor/gestion-evaluadores/items-a-evaluar/items-a-evaluar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-items-a-evaluar',
+  templateUrl: './items-a-evaluar.component.html',
+  styleUrls: ['./items-a-evaluar.component.scss']
+})
+export class ItemsAEvaluarComponent {
+
+}
