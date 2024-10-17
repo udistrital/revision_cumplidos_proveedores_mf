14fea781e900c65d2adc38e85c45bfff79e4070e
stevencol <steven2088@hotmail.es>
2024-10-16 20:20:55 -0500

    feat: Crear componente  de evaluadores
    Se crea el componente de evaluadores
    
    udistrital/Cumplidos_y_certificaciones_documentacion#186

---
+++ src/app/components/evaluacion-proveedor/gestion-evaluadores/evaluadores/evaluadores.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-evaluadores',
+  templateUrl: './evaluadores.component.html',
+  styleUrls: ['./evaluadores.component.scss']
+})
+export class EvaluadoresComponent {
+
+}
