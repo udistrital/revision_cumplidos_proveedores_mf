69699a5afa635d59ca29a6d2bc07788f7ea17832
stevencol <steven2088@hotmail.es>
2024-10-26 02:05:33 -0500

    feat: Editar Formulario
    - Se edita el cuerpo del formulario  para que corresponda a la la solicitud  que da el servicio.
    - Eliminación de la tabla utilizada para pruebas.
    - Se envia  listaCumplidos  a pp-historico-cumplidos
    - Se crea metodo para relizar la peticion
    
    udistrital/Cumplidos_y_certificaciones_documentacion#205

--- src/app/components/historico-cumplidos/formulario-consulta/formulario-consulta.component.ts
+++ src/app/components/historico-cumplidos/formulario-consulta/formulario-consulta.component.ts
@@ -1,42 +1,99 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import Swal from 'sweetalert2';
 import { Month } from 'src/app/models/month.model';
 import { PopUpManager } from 'src/app/managers/popUpManager';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CumplidosProveedoresMidService } from 'src/app/services/cumplidos_proveedores_mid.service';
+import { Cumplido } from 'src/app/models/cumplido';
 
 @Component({
   selector: 'app-formulario-consulta',
   templateUrl: './formulario-consulta.component.html',
-  styleUrls: ['./formulario-consulta.component.css']
+  styleUrls: ['./formulario-consulta.component.css'],
 })
-export class FormularioConsultaComponent  implements OnInit {
+export class FormularioConsultaComponent implements OnInit {
+  @Input() anios: number[] = [];
+  @Input() meses: Month[] = [];
+  @Input() dependencias: any[] = [];
+  @Input() estados: any[] = [];
+  @Output() listaCumplidos = new EventEmitter<Cumplido[]>();
+  title: string = 'CONSULTA CUMPLIDOS APROBADOS';
+  formularioFiltroHistorico!: FormGroup;
+  ListaCumplidos: Cumplido[] = [];
 
-  @Input() anios:number[]=[];
-  @Input() meses:Month[]=[];
-  @Input() dependencias:any[]=[]
-  @Input() estados:any[]=[]
-  title:string="CONSULTA CUMPLIDOS APROBADOS"
-
-  constructor(private popUpManager:PopUpManager){}
-
-  ngOnInit(): void {
+  constructor(
+    private popUpManager: PopUpManager,
+    private fb: FormBuilder,
+    private cumplidosMidService: CumplidosProveedoresMidService
+  ) {
+    this.formularioFiltroHistorico = this.fb.group({
+      anios: [[]],
+      meses: [[]],
+      vigencias: [[]],
+      nombres_proveedor: [[]],
+      estados: [[]],
+      dependencias: [[]],
+      numeros_contrato: [[]],
+    });
   }
 
+  ngOnInit(): void {}
 
-  async consultar(){
+  async consultar() {
+    let peticion = {
+      Anios: this.formularioFiltroHistorico.get('anios')?.value,
+      Meses: this.formularioFiltroHistorico.get('meses')?.value,
+      Vigencias: this.formularioFiltroHistorico.get('vigencias')?.value,
+      Proveedores:
+        this.formularioFiltroHistorico.get('nombres_proveedor')?.value,
+      Estados: this.formularioFiltroHistorico.get('estados')?.value,
+      Dependencias: this.formularioFiltroHistorico.get('dependencias')?.value,
+      Contratos: this.formularioFiltroHistorico.get('numeros_contrato')?.value,
+    };
 
-   let comfirm = await this.popUpManager.showConfirmAlert("¿Estas seguro?","Se va a relizar la busqueda , pude tomar unos momentos")
-
-   if(comfirm.isConfirmed){
-      this.popUpManager.showLoadingAlert("Espera","La busqueda se esta relalizado")
-
-      setTimeout(()=>{
-        Swal.close()
-      },1000);
-
-   }else{
-    this.popUpManager.showErrorAlert("No se realizo ninguna accion")
-   }
+    this.obtenerListadoHistoricos(peticion);
   }
-  
 
+  async obtenerListadoHistoricos(peticion: any) {
+   
+
+   
+    this.popUpManager.showLoadingAlert('Buscando');
+    this.cumplidosMidService
+      .post('/historico-cumplidos/filtro-cumplidos', peticion)
+      .subscribe({
+        next: (response: any) => {
+          this.ListaCumplidos = response.Data.map((cumplido: Cumplido) => {
+            return {
+              NumeroContrato: cumplido.NumeroContrato,
+
+              Vigencia: cumplido.Vigencia,
+              Rp: cumplido.Rp,
+              Mes: cumplido.Mes,
+              FechaCambioEstado: cumplido.FechaCambioEstado,
+              NombreProveedor: cumplido.NombreProveedor,
+              Dependencia: cumplido.Dependencia,
+              Estado: cumplido.Estado,
+              TipoContrato: cumplido.TipoContrato,
+              acciones: [
+                { icon: 'archive', actionName: 'archive', isActive: true },
+                {
+                  icon: 'visibility',
+                  actionName: 'visibility',
+                  isActive: true,
+                },
+              ],
+            };
+          });
+        },
+        error: (error: any) => {
+          console.error('Error');
+          Swal.close();
+        },
+        complete: () => {
+          this.listaCumplidos.emit(this.ListaCumplidos);
+          Swal.close();
+        },
+      });
+  }
 }
