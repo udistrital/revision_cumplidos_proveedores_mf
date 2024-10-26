0483ff700f10291248b6162ca4cacea11570769b
stevencol <steven2088@hotmail.es>
2024-10-26 02:04:00 -0500

    feat: Editar cuerpo de la tabla
    - Se edita el cuerpo de la tabla para que corresponda a la respuesta que da el servicio.
    - Eliminación de la tabla utilizada para pruebas.
    - Se cambia la tabla que se envía a app-listado-historicos.
    - Se recibe la lista de app-formulario-consulta en app-historico-cumplidos.
    
    udistrital/Cumplidos_y_certificaciones_documentacion#205

---
+++ src/app/models/cumplido.ts
@@ -0,0 +1,13 @@
+export interface Cumplido {
+    NumeroContrato: string;
+    Vigencia: string;
+    Rp: string;
+    Mes: number;
+    FechaCambioEstado: string;
+    NombreProveedor: string;
+    Dependencia: string;
+    Estado: string;
+    TipoContrato: string;
+    acciones?:[]
+  }
+  
